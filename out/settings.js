4ba2333fc939b87aefd1a1cab6c705e04ef6e66a
Jean-Sébastien Dupuy <jdupuy@microsoft.com>
2018-06-24 19:38:00 +0200

    separate main settings in a new file

---
+++ out/settings.js
@@ -0,0 +1,10 @@
+"use strict";
+class Settings {
+}
+// Name of Custom Vision Project.
+Settings.PROJECT_NAME = 'Deep Babylon';
+// Path which contain training data.
+Settings.TRAININGDATA_PATH = './data';
+// Batch Size. Limited to 64.
+Settings.BATCH_SIZE = 40;
+//# sourceMappingURL=settings.js.map
